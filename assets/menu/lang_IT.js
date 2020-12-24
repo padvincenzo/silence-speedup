a430817106708a02a197b0839a243d39540e0ade
Vincenzo Padula <32195269+padvincenzo@users.noreply.github.com>
2020-12-24 05:29:06 +0100

    Add files via upload

---
+++ assets/menu/lang_IT.js
@@ -0,0 +1,52 @@
+/*
+Silence SpeedUp
+Speed-up your videos speeding-up (or removing) silences, using FFmpeg.
+This is an electron-based app.
+
+Copyright (C) 2020  Vincenzo Padula
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+module.exports = {
+  file: "&File",
+  openFile: "Importa video",
+  openFolder: "Importa cartella",
+  preferences: "Preferenze",
+  restart: "Ricarica",
+  quit: "Esci",
+  media: "&Media",
+  start: "Avvia",
+  stop: "Interrompi",
+  view: "&Visualizza",
+  progress: "Interfaccia avanzamento",
+  theme: "Tema",
+  lightMode: "Chiaro",
+  darkMode: "Scuro",
+  language: "Lingua",
+  lang_EN: "Inglese",
+  lang_IT: "Italiano",
+  cleanShell: "Svuota shell",
+  toggleDevTools: "Strumenti di sviluppo",
+  help: "&Aiuto",
+  version: "Versione",
+  update: "Aggiornamento disponibile!",
+  about: "Informazioni",
+  license: "Mostra licenza",
+  issue: "Riporta un problema",
+  ref: "Collegamenti",
+  sourceCode: "Codice sorgente",
+  ffmpeg: "FFmpeg",
+  electron: "Electron"
+}
